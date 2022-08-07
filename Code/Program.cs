f3fb6a612ae2d626bab59fe17aeb290b3fa93302
Viktorinkiss <yurkovawika@gmail.com>
2022-08-07 18:00:16 +0500

    Добавили папку с программой и проверили её корректность

---
+++ Code/Program.cs
@@ -0,0 +1,38 @@
+﻿// Написать программу,которая из имеющегося массива строк формирует массив строк, длина которых меньше либо равна 3 симмвола.
+
+void PrintArray(string [] array)//------------------Функция для вывода массива
+{
+    Console.Write("[" + array[0]);
+    for (int y = 1; y < array.Length; y++)
+    {
+        Console.Write(", " + array[y]);
+    }
+    Console.Write("]");
+}
+
+string []  Array (string [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ( array[i].Length <=3 )
+        {
+            count ++;
+        }
+    }
+    string [] newarray = new string [count];
+    int j= 0;
+    for(int i = 0;i < array.Length; i++)
+    {
+        if ( array[i].Length <= 3 )
+        {
+            newarray[j] = array[i];
+            j++;   
+        }
+    }
+    return newarray;
+}
+
+string [] array = { "Moscow", "123" , "Sity" , "abc", "New-York", "Pyt","frt"};
+PrintArray(Array(array));
+
